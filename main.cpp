8d5a843cf0e2f43dc06a291ca66fc114fc00cd4d
Alper Yıldırım <yldrma9@icloud.com>
2019-11-08 10:45:10 +0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -2,15 +2,12 @@
 #include <string>
 using namespace std;
 
-// Returns true if first and last characters
-// of s are same.
 int checkEq(string str){
     return (str[0] == str[str.size() - 1]);
 }
 
 int CountSubstrings(const string& str){
     int result = 0, n = str.length();
-    // Starting point of substring
     for (int i = 0; i < n; i++){
         for (int len = 1; len <= n-i; len++){
             if (checkEq(str.substr(i, len)))
