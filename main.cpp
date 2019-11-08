3c231c5da3766d0fd158151cb3f1919ac07ae100
Alper Yıldırım <yldrma9@icloud.com>
2019-11-08 10:49:47 +0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-int checkEq(string str){
-    return (str[0] == str[str.size() - 1]);
+int checkEq(string basicString){
+    return (basicString[0] == basicString[basicString.size() - 1]);
 }
 
-int CountSubstrings(const string& str){
-    int result = 0, n = str.length();
+int CountSubstrings(const string& string1){
+
+    int result = 0, n = string1.length();
     for (int i = 0; i < n; i++){
-        for (int len = 1; len <= n-i; len++){
-            if (checkEq(str.substr(i, len)))
+        for (int length = 1; length <= n - i; length++){
+            if (checkEq(string1.substr(i, length)))
                 result++;
         }
     }
+
     return result;
 }
 
 int main(){
+
     string input;
     cout << "Enter a string: ";
     cin >> input;
